ee0e205259f6de6b1a685b4c35cab124746d5fea
Calibion <compte.de.gromega00@gmail.com>
2019-06-16 23:10:50 +0200

    ça a l'air de marcher

--- Main.py
+++ Main.py
@@ -22,18 +22,28 @@ Plateau = Plateau() #initialise l'objet plateau (Matrice, nbCasesX, nbCasesY, ta
 #Creation et affichage de la fenetre:
 initFenetre(Plateau,pygame)
 
-quitter = 0
+quitter = True
+
+#test Calibion
+nbCasesX = 41
+nbCasesY = 21
+tailleFenetre = (nbCasesX*30 + 50,nbCasesY*30 + 50)
+matrice = creationMatrice(tailleFenetre, nbCasesX, nbCasesY)
+test = Mob(matrice)
+print(f"{test.posxmatrice},{test.posymatrice}")
+test.move_to_next_pos()
+print(f"{test.posxmatrice},{test.posymatrice}")
 
 
 #__________________boucle principale:________________
 
-while quitter == 0: #tout ce passe là dedans
+while quitter: #tout ce passe là dedans
     
     for event in pygame.event.get(): #il passe toutes les touches en revu pour voir lorsque tu appuies sur une touche
         pygame.event.pump() #c'est pour pas qu'il fasse "le programme ne répond plus", normalement ça marche sans mais pour moi non
             
         if event.type == pygame.QUIT: #quand t'appuies sur la croix ça quitte :)
-            quitter = 1
+            quitter = False
 
 
     pygame.display.flip() #rafraichit l'image !
--- Mob.py
+++ Mob.py
@@ -1,18 +1,48 @@
-
+from random import randint
+direction = [(-1,0),(0,1),(1,0),(0,-1)]
 
 class Mob:
 
-    def __init__(self,name,posx,posy,pv):
-        self.name = name
-        self.posx = posx
-        self.posy = posy
+    def __init__(self ,matrice ,name = "mobBase",speed = 100 ,pv = 100 ,wasat = 3):
+        self.matrice = matrice #le mob a en memoire la map, y'a surement mieux en vrai
+        for posy in range(len(matrice)):
+            for posx in range(len(matrice[posy])):
+                if matrice[posy][posx] == 2:
+                    self.posxmatrice = posx
+                    self.posymatrice = posy
+                    self.posx = posx * 30 + 15
+                    self.posy = posy * 30 + 15
+        self.speed = speed 
         self.pv = pv
-
-    def isitdead(self):
-        if self.pv <= 0 :
-            print("{} is dead".format(self.name))
-            return True
-        else:
-            return False
-
-#test2
+        self.name = name
+        posxmax = len(matrice[0])
+        posymax = len(matrice)
+        self.wasat = wasat
+        
+    def move_to_next_pos(self):
+        directiondispo = dict()
+        for i in range(4):
+            (y,x) = direction[i]
+            if i == self.wasat :
+                pass
+            elif self.matrice[self.posymatrice + y][self.posxmatrice + x] == 3:
+                self.posxmatrice += x
+                self.posymatrice += y
+                self.posx = self.posxmatrice * 30 - 15
+                self.posy = self.posymatrice * 30 - 15
+                #self.finnishthematrice
+            elif self.matrice[self.posymatrice + y][self.posxmatrice + x] == 1:
+                directiondispo[i] = direction[i]
+            else:
+                pass
+        try:
+            randomdirection = randint(0,len(directiondispo)-1)
+            (y,x) = list(directiondispo.values())[randomdirection]
+            self.posxmatrice += x
+            self.posymatrice += y
+            self.posx = self.posxmatrice * 30 - 15
+            self.posy = self.posymatrice * 30 - 15
+            wasat = list(directiondispo.keys())[randomdirection]     
+        except ValueError:
+            print(f"{self.name} is stuck at ({self.posxmatrice},{self.posymatrice})")
+                

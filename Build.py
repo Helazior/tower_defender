3ba06886bfe5de6188ea8ac81d5ff07f23a07d13
Helazior <51758632+Helazior@users.noreply.github.com>
2019-06-17 15:15:07 +0200

    Update Build.py

--- Build.py
+++ Build.py
@@ -1,33 +1,36 @@
 from math import sqrt
-#test
-class Build :
-#test2
-    #lalali je fais un test
-    def  __init__(self,name,posx,posy,brange,damage):
-        self.name = name
-        self.posx = posx
-        self.posy = posy
-        self.ticsincelastaction = 1
-        self.range = brange
-        self.dmg = damage
+from Mob import Mob
 
-    def attack(self,listfoes):
-        if listfoes == []:
+def convertPixMatrice(pos):
+    return (int(pos[0]/30), int(pos[1]/30))
+
+class Build :
+    def  __init__(self, pygame, Plateau, pos, brange = 100, damage = 10, taille = 2):
+        self.name = name
+        self.posx = pos[0]
+        self.posy = pos[1]
+        self.cadence = 1
+        self.range = brange
+        self.damage = damage
+
+        for i in range(taille):
+            for j in range(taille):
+                Plateau.Matrice[j][i] = 9 #mettre images
+
+    def attack(self,listMob):
+        if listMob == []:
             pass
         else:
-            for i in range(len(listfoes)) :
-                foe = listfoes[i]
-                distance = sqrt(((self.posx - foe.posx)**2)+((self.posy - foe.posy)**2))
+            for i in range(len(listMob)) :
+                mob = listMob[i]
+                distance = sqrt(((self.posx - mob.posx)**2)+((self.posy - mob.posy)**2))
                 if  distance <= self.range :
                     
-                    foe.pv -= self.dmg
-                    print("{} deal {} dmg to {}".format(self.name,self.dmg,foe.name))
+                    mob.pv -= self.damage
+                    print(f"{self.name} deal {self.damage} damage to {mob.name}")
                     print(i)
-                    #super test
 
                     
-                    if foe.isitdead():
-                         listfoes.pop(i)
+                    if mob.pv <= 0:
+                         listMob.pop(i)
                     break
-
-#coucou

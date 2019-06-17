bdf325a8da778a4a7bd1521949ccfcc94894567b
Helazior <51758632+Helazior@users.noreply.github.com>
2019-06-17 11:16:15 +0200

    Update Plateau.py

--- Plateau.py
+++ Plateau.py
@@ -1,6 +1,7 @@
-def initFenetre(Plateau, pygame): #crée et affiche la fenêtre
+def initFenetre(pygame, nbCasesX, nbCasesY,tailleFenetre, Matrice, margeX, margeY): #crée et affiche la fenêtre
     pygame.display.set_caption("Tower Defender !")
-    fenetre = pygame.display.set_mode(Plateau.tailleFenetre, pygame.RESIZABLE)
+
+    fenetre = pygame.display.set_mode(tailleFenetre)
 
     #je charge et convertis les images dans des variables
     fond = pygame.image.load("fond.png").convert()
@@ -8,22 +9,22 @@ def initFenetre(Plateau, pygame): #crée et affiche la fenêtre
     start = pygame.image.load("start.png").convert_alpha()
     end = pygame.image.load("end.png").convert_alpha()
 
-
-    fond = pygame.transform.scale(fond, Plateau.tailleFenetre) #l'image de fond fait la taille de la fenêtre
+    fond = pygame.transform.scale(fond, tailleFenetre) #l'image de fond fait la taille de la fenêtre
     fenetre.blit(fond, (0,0)) #affiche le fond à la position (0,0)
-    for i in range(Plateau.nbCasesX):
-        for j in range(Plateau.nbCasesY):
-            if Plateau.Matrice[j][i] != 0:
-                fenetre.blit(chemin, (30*i,30*j)) #on place un chemin quand c'est pas le fond
-            if Plateau.Matrice[j][i] == 2:
-                fenetre.blit(start, (30*i,30*j)) #on place le start
-            elif Plateau.Matrice[j][i] == 3:
-                fenetre.blit(end, (30*i,30*j))  #on place l'arrivé des mobs
 
-    fenetre_fond = pygame.display.get_surface().copy() #Copie du fond pour faire bouger les trucs devant
+    for i in range(nbCasesX):
+        for j in range(nbCasesY):
+            if Matrice[j][i] != 0:
+                fenetre.blit(chemin, (30*i + margeX, 30*j + margeY)) #on place un chemin quand c'est pas le fond
+            if Matrice[j][i] == 2:
+                fenetre.blit(start, (30*i + margeX, 30*j + margeY)) #on place le start
+            elif Matrice[j][i] == 3:
+                fenetre.blit(end, (30*i + margeX, 30*j + margeY))  #on place l'arrivé des mobs
 
     pygame.display.flip() #rafraichit l'image !
 
+    copy_fond = pygame.display.get_surface().copy() #Copie du fond pour faire bouger les trucs devant
+    return fenetre, copy_fond
 
 
 
@@ -40,7 +41,10 @@ def creationMatrice(tailleFenetre, nbCasesX, nbCasesY):
 
         for i in range(nbCasesX):
             for j in range(nbCasesY):
-                Matrice[j][i] = int(liste_grille[j][i])
+                try:
+                    Matrice[j][i] = int(liste_grille[j][i])
+                except:
+                    print("ERREUR, le fichier texte de la map n'est pas de la bonne dim")
                 
     return Matrice
     
@@ -48,16 +52,23 @@ def creationMatrice(tailleFenetre, nbCasesX, nbCasesY):
 
 
 
-class Plateau: #classe de la map attributs: Matrice, nbCasesX, nbCasesY, tailleFenetre
+class Plateau: #classe de la map attributs: Matrice, nbCasesX, nbCasesY, tailleFenetre, fenetre
 
-    def __init__(self):
-        nbCasesX = 41
-        nbCasesY = 21
-        tailleFenetre = (nbCasesX*30 + 50,nbCasesY*30 + 50)
+    def __init__(self, pygame):
+        nbCasesX = 40
+        nbCasesY = 20
+        tailleFenetre = (nbCasesX*30 + 80,nbCasesY*30 + 80)
         
         self.nbCasesX = nbCasesX
         self.nbCasesY = nbCasesY
         self.tailleFenetre = tailleFenetre
+        Matrice = creationMatrice(tailleFenetre, nbCasesX, nbCasesY)
+        self.Matrice = Matrice
+
+        margeFenetreGauche = 15
+        margeFenetreHaut = 15
+
+        self.margeFenetreGauche = margeFenetreGauche
+        self.margeFenetreHaut = margeFenetreHaut
         
-        self.Matrice = creationMatrice(tailleFenetre, nbCasesX, nbCasesY)
-        
+        self.fenetre, self.copy_fond = initFenetre(pygame, nbCasesX, nbCasesY,tailleFenetre, Matrice, margeFenetreGauche, margeFenetreHaut)
